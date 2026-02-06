65e1560d969fda59c3a92e07fbfc0a640edfb154
JBacule <44210913+jbacule@users.noreply.github.com>
2026-02-06 14:24:59 +0800

    add seo

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://chromewebstore.google.com/detail/scrolly/cljgkkbcdmljgocmchgmgcjnppbcfiii"
+  site: "https://scrolly.jbacule.dev",
+  integrations: [sitemap()]
 });
